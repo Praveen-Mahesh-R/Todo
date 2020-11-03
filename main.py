9414cfcbee2eba783121b8d2c5f7eab6777f6aff
Praveen-Mahesh-R <praveen.adyar@gmail.com>
2020-11-03 20:52:30 +0530

    todolist (file system)

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import PySimpleGUI as sg
+from os import path
 
 def add_task(values):
     task = values['taskname']
-    todolist.append(task)
-    window.FindElement('taskname').Update(value="")
-    window.FindElement('todolist').Update(values=todolist)
-    window.FindElement('add_save').Update('Add')
+    if task not in todolist:
+        todolist.append(task)
+        window.FindElement('taskname').Update(value="")
+        window.FindElement('todolist').Update(values=todolist)
+        window.FindElement('add_save').Update('Add')
+    else:
+        window.FindElement('taskname').Update(value="")
 
 def edit_tasks(values):
     edit_val = values['todolist'][0]
@@ -18,17 +22,38 @@ def delete_tasks(values):
     todolist.remove(delete_val)
     window.FindElement('todolist').Update(values=todolist)
 
-todolist=[]
+def check_file():
+    value = path.exists("listfile.txt")
+    return value
+
+def update_file(values):
+        with open("listfile.txt",'w') as file:
+            for task in todolist:
+                file.write("%s\n" % task)
+
+def find_file_values():
+    templist = []
+    if c == True:
+        with open("listfile.txt", 'r') as file:
+            for content in file:
+                templist.extend(content.split())
+    return templist
+
+c = check_file()
+
+todolist = find_file_values()
 
 layout = [
     [sg.Text('Enter the task'),sg.InputText("", key='taskname'),
     sg.Button("Add",key="add_save")],
-    [sg.Listbox(values=[], size=(30,10), key='todolist'),
+    [sg.Listbox(values=todolist, size=(30,10), key='todolist'),
      sg.Button("Edit"),
-     sg.Button("Delete")]
+     sg.Button("Delete")],
+    [sg.Button("Save and Quit")]
     ]
 
 window = sg.Window('TodoList', layout)
+
 while True:
     button, values = window.Read()
     if button == 'add_save':
@@ -37,6 +62,9 @@ while True:
         edit_tasks(values)
     elif button == 'Delete':
         delete_tasks(values)
+    elif button == "Save and Quit":
+        update_file(values)
+        break
     else:
         break
 window.Close()
