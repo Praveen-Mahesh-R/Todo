a78abf38ff492a192a2cb9ba87963a0ad1d7c54d
Praveen-Mahesh-R <praveen.adyar@gmail.com>
2020-11-03 17:47:29 +0530

    todolist (edit and delete tasks)

--- main.py
+++ main.py
@@ -7,21 +7,36 @@ def add_task(values):
     window.FindElement('todolist').Update(values=todolist)
     window.FindElement('add_save').Update('Add')
 
+def edit_tasks(values):
+    edit_val = values['todolist'][0]
+    window.FindElement('taskname').Update(value=edit_val)
+    todolist.remove(edit_val)
+    window.FindElement('add_save').Update('Save')
+
+def delete_tasks(values):
+    delete_val = values['todolist'][0]
+    todolist.remove(delete_val)
+    window.FindElement('todolist').Update(values=todolist)
+
 todolist=[]
+
 layout = [
     [sg.Text('Enter the task'),sg.InputText("", key='taskname'),
     sg.Button("Add",key="add_save")],
-    [sg.Listbox(values=[], size=(30,10), key='todolist')],
+    [sg.Listbox(values=[], size=(30,10), key='todolist'),
+     sg.Button("Edit"),
+     sg.Button("Delete")]
+    ]
 
-]
 window = sg.Window('TodoList', layout)
 while True:
     button, values = window.Read()
     if button == 'add_save':
         add_task(values)
-    #elif button == 'Exit':
-    #    sg.Popup('Names are:',
-    #    break
+    elif button == 'Edit':
+        edit_tasks(values)
+    elif button == 'Delete':
+        delete_tasks(values)
     else:
         break
 window.Close()
